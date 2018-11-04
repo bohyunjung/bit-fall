f5bbaaad51ecb0de645f91fc6fc9da4c7836d2e8
Bohyun Jung <orange.b.jung@gmail.com>
2018-11-05 03:30:35 +0900

    Update file names

--- static/expression.js
+++ static/expression.js
@@ -1,3 +1,30 @@
+class ParticleSystem {
+  constructor() {
+    this.dots = [];
+    for (let i = W/(N_NOZZLES+1); i < W; i+=W/(N_NOZZLES+1)) {
+        this.dots.push([i, 0]);
+    }
+    this.particles = [];
+  }
+  
+  addParticle(x, y) {
+    this.particles.push(new Particle(x, y));
+  }
+
+  applyForce(f) {
+    for (let particle of this.particles) {
+      particle.applyForce(f);
+    }
+  }
+
+  update() {
+    for (let particle of this.particles) {
+      particle.run();
+    }
+    this.particles = this.particles.filter(particle => !particle.isDead());
+  }
+}
+
 class Particle {
   constructor(x, y) {
     this.acc = createVector(0, 0);
